b36d579f76d43bb80e1a03f06b2a2920267e7e8c
Matthieu Bilbille <matthieu.bilbille@gmail.com>
2013-02-24 01:42:28 +0100

    Improve options page

--- resources/js/options.js
+++ resources/js/options.js
@@ -6,27 +6,65 @@
  */
 
  var MailCheckOptions = {
+    domains: Kicksend.mailcheck.defaultDomains,
+    topLevelDomains: [],
+    selectors: ['textarea', 'input[type="text"]', 'input[type="email"]'],
     init: function(){
-        document.addEventListener('DOMContentLoaded', MailCheckOptions.restore);
-        document.querySelector('#save').addEventListener('click', MailCheckOptions.save);
+        document.addEventListener('DOMContentLoaded', MailCheckOptions.load(function(){
+            MailCheckOptions.restore();
+        }));
+        $(document).on('blur', ['textarea'], function(e){
+                MailCheckOptions.save();
+        });
+        $(document).on('click', "#showAdvanced", function(e) {
+            $(".advanced").toggle();
+            $("#showAdvanced").hide();
+            $("#hideAdvanced").show();
+        });$(document).on('click', "#hideAdvanced", function(e) {
+            $(".advanced").toggle();
+            $("#hideAdvanced").hide();
+            $("#showAdvanced").show();
+        });
     },
     save: function(){
-        var domains = $("#domains").val().replace(/\s+/g, '').split(',');
-
-        // Update status to let user know options were saved.
-        chrome.storage.sync.set({'domains': domains}, function() {
-            var status = document.getElementById("status");
-            status.innerHTML = "Options Saved.";
+        // Retrieve data
+        MailCheckOptions.domains = $("#domains").val().replace(/\s+/g, '').split(',');
+        MailCheckOptions.topLevelDomains = $("#topLevelDomains").val().replace(/\s+/g, '').split(',');
+        MailCheckOptions.selectors = $("#selectors").val().replace(/\s+/g, '').split(',');
+        
+        // ... and save it
+        var data = {
+            'domains': MailCheckOptions.domains,
+            'topLevelDomains': MailCheckOptions.topLevelDomains,
+            'selectors': MailCheckOptions.selectors,
+        };
+        chrome.storage.sync.set(data, function() {
+            // Update status to let user know options were saved.
+            var status = $("#status");
+            status.html("Options Saved.");
             setTimeout(function() {
-                status.innerHTML = "";
+                status.html("");
             }, 750);
         });
     },
-    restore: function(){
-        chrome.storage.sync.get('domains', function(items){
-            document.getElementById("domains").innerHTML = items.domains.join(', ');
+    load: function(callback){
+        chrome.storage.sync.get(['domains', 'topLevelDomains', 'selectors'], function(items){
+            for(var i in items) {
+                MailCheckOptions[i] = items[i];
+            }
+
+            if(typeof callback == "function"){
+                callback();
+            }
         });
+    },
+    restore: function(){
+        $("#domains").val(MailCheckOptions.domains.join(', '));
+        $("#topLevelDomains").val(MailCheckOptions.topLevelDomains.join(', '));
+        $("#selectors").val(MailCheckOptions.selectors.join(', '));
     }
 };
 
-MailCheckOptions.init();
+if($("body").attr('page') === "options"){
+    MailCheckOptions.init();
+}
--- script.js
+++ script.js
@@ -3,33 +3,21 @@
  * Author
  * Matthieu Bilbille (@bilubilu28)
  *
- * License
- * Copyright (c) 2012 Receivd, Inc.
- *
  * Licensed under the MIT License.
  *
- * v 1.2
+ * v 1.3
  **/
 
  var MailCheck = {
-    elementTypes: ['textarea', 'input[type="text"]', 'input[type="email"]'],
-    domains: [],
     init: function(){
-        $(document).on('blur', MailCheck.elementTypes, function(e){
+        MailCheckOptions.load(function(){
+            $(document).on('blur', MailCheckOptions.selectors, function(e){
                 MailCheck.run(this);
-        });
-        chrome.storage.sync.get('domains',function(items){
-            if(!items.domains){
-                MailCheck.domains = Kicksend.mailcheck.defaultDomains;
-                chrome.storage.sync.set({'domains': Kicksend.mailcheck.defaultDomains});
-            }
-            else{
-                MailCheck.domains = items.domains;
-            }
+            });
         });
     },
     run: function(element) {
-        var re = /\b([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,5})\b/gi;
+        var re = /\b([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6})\b/gi;
         var emails = element.value.match(re);
         for(var i in emails) {
             if(i == "index" || i == "input") {
@@ -37,7 +25,8 @@
             }
             Kicksend.mailcheck.run ({
                 email: emails[i],
-                domains: MailCheck.domains,
+                domains: MailCheckOptions.domains,
+                topLevelDomains: MailCheckOptions.topLevelDomains,
                 suggested: function(suggestion) {
                     chrome.extension.sendMessage({suggestion: suggestion.full});
                 },
